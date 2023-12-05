70b5f8df9e828a757879c4db80a4a8dc0fbedaca
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-04 22:43:34 -0600

    completed day1 part 2

--- Day1/second.py
+++ Day1/second.py
@@ -1,69 +1,30 @@
-
 ## first.py
+import re
 
-# def process_line(line):
-#     number_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-#     numbers = [int(s) for s in line if s.isdigit()]  # Extract numbers from the line
-#     print(f"Numbers: {numbers}")
-#     if numbers:  # Check if numbers are present
-#         first_number = numbers[0]
-#         last_number = numbers[-1]
-#         calibration_value = str(first_number) + str(last_number)
-#         print(f"Clibration Value: {calibration_value}")
-#         print(first_number + last_number)
-#         return int(calibration_value)
-#     return 0  # Return 0 if no numbers found in the line
+with open("./inputD1n.txt") as f:
+    data = f.read().strip()
 
 
-def process_line(line):
-    number_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    
-    # Extract numbers from the line
-    numbers = [int(s) for s in line if s.isdigit()]
-    print(f"Numbers: {numbers}")
-
-    # Extract words representing numbers from the line
-    words = line.split()
-    words = [word.lower() for word in words]  # Convert words to lowercase for comparison
-
-    for word in words:
-        if word in number_words:
-            number_index = number_words.index(word) + 1  # Get the index of the word in the list
-            numbers.append(number_index)  # Append the corresponding number to the numbers list
-
-    print(f"Numbers after word search: {numbers}")
-
-    if numbers:  # Check if numbers are present
-        first_number = numbers[0]
-        last_number = numbers[-1]
-        calibration_value = str(first_number) + str(last_number)
-        print(f"Calibration Value: {calibration_value}")
-        print(first_number + last_number)
-        return int(calibration_value)
-    return 0  # Return 0 if no numbers found in the line
-
-def read_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            calibration_list = []
-            counter = 0
-            for line in file:
-                counter += 1
-                calibration_value = process_line(line)
-                if calibration_value != 0:
-                    calibration_list.append(calibration_value)
-                
-            print(f"sum, {sum(calibration_list)}")
-            return sum(calibration_list)
-            
-    except FileNotFoundError:
-        print("File not found.")
-    except IOError as e:
-        print("Error reading the file:", e)
-
-# Replace 'path/to/your/file.txt' with the actual file path
-file_path = './inputD1n.txt'
-read_file(file_path)
+def calibration(data):
+    ls = data.split("\n")
+    ns = [re.findall("\d", x) for x in ls]
+    return sum(int(n[0] + n[-1]) for n in ns)
 
 
+# Part 1
+print(calibration(data))
 
+# Part 2
+data = (
+    data.replace("one", "one1one")
+    .replace("two", "two2two")
+    .replace("three", "three3three")
+    .replace("four", "four4four")
+    .replace("five", "five5five")
+    .replace("six", "six6six")
+    .replace("seven", "seven7seven")
+    .replace("eight", "eight8eight")
+    .replace("nine", "nine9nine")
+)
+
+print(calibration(data))
