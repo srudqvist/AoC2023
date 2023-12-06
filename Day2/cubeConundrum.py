4ca5017309453d5563cdd6069c4955db9b349011
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-06 11:54:58 -0600

    day 2 part 2 wip

--- Day2/cubeConundrum.py
+++ Day2/cubeConundrum.py
@@ -5,38 +5,66 @@
 # 12 red cubes, 13 green cubes and 14 blue cubes.
 
 
-
 max_cubes = {
-    "red":12,
-    "green":13,
-    "blue":14,
+    "red": 12,
+    "green": 13,
+    "blue": 14,
 }
 
 
 def determine_game(line):
-    game, sets = line.split(': ')
-    game, game_number = game.split(' ')
-    sets = sets.split('; ')
+    game_invalid = False
+    game, sets = line.split(": ")
+    game, game_number = game.split(" ")
+    sets = sets.split("; ")
+    min_cubes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
     for current_set in sets:
-        current_set = current_set.split(', ')
+        current_set = current_set.split(", ")
         for cubes in current_set:
-            value, color = cubes.strip().split(' ')
+            value, color = cubes.strip().split(" ")
             if max_cubes[color] < int(value):
                 print(f"Returning False: {cubes}")
-                return 0
-    return int(game_number)
+                game_invalid = True
+
+            if min_cubes[color] == 0:
+                min_cubes[color] = int(value)
+            if min_cubes[color] > int(value):
+                min_cubes[color] = int(value)
+    print(min_cubes)
+    power = 1
+    for value in min_cubes.values():
+        if value != 0:
+            power *= value
+    print(f"Power: {power}")
+    if game_invalid:
+        return 0, power
+    else:
+        return int(game_number), power
+
 
 def read_file(file_path):
     try:
         total = 0
-        with open(file_path, 'r') as file:
+        total_power = 0
+        with open(file_path, "r") as file:
             for line in file:
                 print(line)
-                total += determine_game(line)
-            print(total)
+                local_total, power = determine_game(line)
+                total += local_total
+                total_power += power
+            print(f"total: {total}")
+            print(f"total_power: {total_power}")
+            # 642 is incorrect, 2143
     except FileNotFoundError:
         print("File not found.")
     except IOError as e:
         print("Error reading the file:", e)
 
-read_file("./day2Input.txt")
+
+read_file("Day2\day2Input.txt")
+
+# read_file("./day2Input.txt")
