16dd9987f63f33a2567485548a008721884f3dea
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-08 22:36:30 -0600

    Day 5 part 2 finished

--- Day5/fertilizer.py
+++ Day5/fertilizer.py
@@ -1,23 +1,55 @@
 # fertilizer.py
 
+def partOne():
+    seeds, *blocks = open("./day5Input.txt").read().split("\n\n")
+    seeds = list(map(int, seeds.split(":")[1].split()))
 
+    for block in blocks:
+        ranges = []
+        for line in block.splitlines()[1:]:
+            ranges.append(list(map(int, line.split())))
+        new = []
+        for x in seeds:
+            for a,b,c in ranges:
+                if b <= x < b + c:
+                    new.append(x - b + a)
+                    break
+            else:
+                new.append(x)
+        seeds = new
+    print(min(seeds))
+          
+def partTwo():
 
-seeds, *blocks = open("./day5Input.txt").read().split("\n\n")
+    inputs, *blocks = open("./day5Input.txt").read().split("\n\n")
+    inputs = list(map(int, inputs.split(":")[1].split()))
 
-seeds = list(map(int, seeds.split(":")[1].split()))
+    seeds = []
+    for i in range(0, len(inputs), 2):
+        seeds.append((inputs[i], inputs[i] + inputs[i + 1]))
 
-for block in blocks:
-    ranges = []
-    for line in block.splitlines()[1:]:
-        ranges.append(list(map(int, line.split())))
-    new = []
+    for block in blocks:
+        ranges = []
+        for line in block.splitlines()[1:]:
+            ranges.append(list(map(int, line.split())))
+        new = []
+        while len(seeds) > 0:
+            s,e = seeds.pop()
+            for a, b, c in ranges:
+                os = max(s,b)
+                oe = min(e, b + c)
+                if os < oe:
+                    new.append((os - b + a, oe - b + a))
+                    if os > s:
+                        seeds.append((s, os))
 
-    for x in seeds:
-        for a,b,c in ranges:
-            if b <= x < b + c:
-                new.append(x - b + a)
-                break
-        else:
-            new.append(x)
-    seeds = new
-print(min(seeds))
+                    if e > oe:
+                        seeds.append((oe, e))
+                    break
+            else:
+                new.append((s, e))
+        seeds = new
+    print(min(seeds)[0])
+
+partOne()
+partTwo()
