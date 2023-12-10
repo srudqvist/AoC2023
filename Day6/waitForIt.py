637d582889b09e6060c8b03ca9d0821a5ea5731b
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-09 21:54:56 -0600

    finished day 6 part 1

---
+++ Day6/waitForIt.py
@@ -0,0 +1,64 @@
+# faitForIt.py
+def first(content):
+    time, distance = content.strip().split("\n")
+
+    time = list(map(int, time.split(":")[1].strip().split()))
+    distance = list(map(int, distance.split(":")[1].strip().split()))
+
+    counts = []
+    for i in range(len(time)):
+        t = time[i]
+        d = distance[i]
+        count = 0
+        for i in range(0, t):
+            my_dist = i * (t - i)
+            if my_dist > d:
+                count += 1
+        counts.append(count)
+    result = 1
+    for num in counts:
+        result *= num
+    print(f"Result: {result}")
+
+def second(content):
+    time, distance = content.strip().split("\n")
+    
+    time = list(map(int, time.split(":")[1].strip().split()))
+    time = int(''.join(map(str, time)))
+    print(time)
+
+    distance = list(map(int, distance.split(":")[1].strip().split()))
+    distance = int(''.join(map(str, distance)))
+    print(distance)
+    
+    counts = []
+    for i in range(len(time)):
+        t = time[i]
+        d = distance[i]
+        count = 0
+        for i in range(0, t):
+            my_dist = i * (t - i)
+            if my_dist > d:
+                count += 1
+        counts.append(count)
+    result = 1
+    for num in counts:
+        result *= num
+    print(f"Result: {result}")
+
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            # for line in file:
+            # data = f.read().strip()
+            content = file.read()
+            first(content)
+            second(content)
+    except FileNotFoundError:
+        print("File not found.")
+    except IOError as e:
+        print("Error reading the file:", e)
+
+read_file("./day6Input.txt")
+#read_file("./testInput.txt")
