44c86bf218abaa501c310c4e647f4612f6c9526d
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-10 22:13:42 -0600

    day 7 progress

--- Day7/camelCards.js
+++ Day7/camelCards.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 
-let lines = fs.readFileSync("./testInput.txt").toString().split("\n");
-let [hands, bids] = fs.readFileSync("./testInput.txt").toString().split("\n");
-//let [hands2, bids2] = readFile("./testInput.txt").toString().split("\n");
-console.log(`Hands: ${hands} Bids: ${bids}`);
+let lines = fs.readFileSync("./testInput.txt").toString().trim().split("\n");
 
 //console.log(`Hands2: ${hands2} Bids2: ${bids2}`);
 
 for (let i = 0; i < lines.length; i++) {
   console.log(`Lines: ${lines[i]}`);
+}
+
+const hands = [];
+lines.forEach((line) => {
+  const [cards, bid] = line.split(" ");
+  const hand = {
+    cards: cards,
+    bid: parseInt(bid),
+  };
+  hands.push(hand);
+});
+
+for (let i = 0; i < hands.length; i++) {
+  console.log(`Hands: ${hands[i].cards}, ${hands[i].bid}`);
 }
