7bbaab4933027e34b8a8c8d9ae40cfc1cf15111e
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-11 21:15:26 -0600

    Day7 fixes in detecting type

--- Day7/camelCards.js
+++ Day7/camelCards.js
@@ -7,13 +7,12 @@ let lines = fs.readFileSync("./testInput.txt").toString().trim().split("\n");
 for (let i = 0; i < lines.length; i++) {
   console.log(`Lines: ${lines[i]}`);
 }
-
+const order = ["A", "K", "Q", "J", "T", 9, 8, 7, 6, 5, 4, 3, 2];
 const hands = [];
 lines.forEach((line) => {
   const [cards, bid] = line.split(" ");
-  let type;
+  let type = detectType(cards);
   let rank;
-  detectType(cards);
 
   const hand = {
     cards: cards,
@@ -22,12 +21,40 @@ lines.forEach((line) => {
     rank: rank,
   };
   hands.push(hand);
+  console.log(`Hand: ${hand.cards}, ${hand.bid}\n${hand.type}\n`);
 });
 
+
 function detectType(cards) {
-  console.log(`Cards: ${cards}`);
   cards = cards.split("").sort().join("");
-  console.log(`Cards: ${cards}`);
-  console.log("\n");
-  for (let i = 0; i < cards.length - 1; i++) {}
+  // for (let i = 0; i < cards.length - 1; i++) {
+  //
+  // }
+  const frequency = {};
+  for (const card of cards) {
+    frequency[card] = (frequency[card] || 0) + 1;
+  }
+  const counts = Object.values(frequency);
+  const pairs = counts.filter((count) => count === 2).length;
+  const threeOfAKind = counts.includes(3);
+  const fourOfAKind = counts.includes(4);
+  const fiveOfAKind = counts.includes(5);
+  const fullHouse;
+  if (fiveOfAKind) {
+    return 5;
+  } else if (fourOfAKind){
+    return 4;
+  } else if (threeOfAKind) {
+    return 3;
+  } else if (pairs === 2) {
+    return 2;
+  } else if (pairs === 1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function assignRank(hands) {
+
 }
