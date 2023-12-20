061f96121c47e3ef32f2d2420b134cb50c5c6e29
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-20 11:50:38 -0600

    Day 8 part 1 completed

--- Day8/hauntedWasteland2.js
+++ Day8/hauntedWasteland2.js
@@ -1,29 +1,16 @@
-// hauntedWasteland2.js
-//
-
-// follow the LR pattern to navigate from AAA to ZZZ.
 // how many steps are needed?
 
 const fs = require("fs");
 
-//let lines = fs.readFileSync("./testInput2.txt").toString().trim().split("\n");
-//let lines = fs.readFileSync("./testInput.txt").toString().trim().split("\n");
-// let lines = fs
-//   .readFileSync("./testInputPart2.txt")
-//   .toString()
-//   .trim()
-//   .split("\n");
 let lines = fs.readFileSync("./day8Input.txt").toString().trim().split("\n");
 const path = lines.shift();
-console.log(path);
 
 // remove the empty line
 lines.shift();
 
-const goal = "ZZZ";
 const nodes = {};
 let startNodes = {};
-let endNodes = {};
+let stepList = [];
 
 lines.forEach((line) => {
   console.log(line);
@@ -34,73 +21,59 @@ lines.forEach((line) => {
   if (key.endsWith("A")) {
     startNodes[key] = values;
   }
-  if (key.endsWith("Z")) {
-    endNodes[key] = values;
-  }
 });
-console.log(nodes);
-console.log("START NODES\n", startNodes);
-console.log("\nEND NODES\n", endNodes);
-let endKeys = [];
-for (const endNode in endNodes) {
-  endKeys.push(endNode);
+
+for (const startNode in startNodes) {
+  stepList.push(findZZZ(nodes, path, startNode));
 }
 
-findZZZ(nodes, path, goal, startNodes);
+const result = calculateLCM(stepList);
+console.log(`Total steps: ${result}`);
 
-function findZZZ(nodes, path, goal, startNodes) {
+function findZZZ(nodes, path, startNode) {
   let steps = 0;
   let goalFound = false;
-  let currentNodes = startNodes;
-  console.log("Current Nodes: ", currentNodes);
+  let currentNode = startNode;
 
   while (!goalFound) {
     for (let i = 0; i < path.length; i++) {
-      let newCurrentNodes = {};
-      let currentKeys = [];
-      for (const myCurrentNode in currentNodes) {
-        let LR_values = currentNodes[myCurrentNode];
-        //console.log(`Current Node: ${myCurrentNode}`);
-        //console.log("LR_values:", LR_values);
-        if (path[i] == "L") {
-          // go to the left
-          let nextNodeKey = LR_values[0];
-          let nextNodeValues = nodes[LR_values[0]];
-          //console.log(`Next Node: ${nextNodeKey}: [${nextNodeValues}]`);
-          newCurrentNodes[nextNodeKey] = nextNodeValues;
-        } else if (path[i] == "R") {
-          // go to the right
-          //console.log("RIGHT");
-          let nextNodeKey = LR_values[1];
-          let nextNodeValues = nodes[LR_values[1]];
-          //console.log(`Next Node: ${nextNodeKey}: [${nextNodeValues}]`);
-          newCurrentNodes[nextNodeKey] = nextNodeValues;
-        }
-        currentKeys.push(myCurrentNode);
+      if (path[i] == "L") {
+        // go to the left
+        currentNode = nodes[currentNode][0];
+      } else if (path[i] == "R") {
+        // go to the right
+        currentNode = nodes[currentNode][1];
       }
-      currentNodes = newCurrentNodes;
       steps++;
-      // for (const currentNode in currentNodes) {
-      //   currentKeys.push(currentNode);
-      // }
-      //console.log(`Current keys: ${currentKeys}\nEnd keys: ${endKeys}`);
-      let matchCount = 0;
-      if (steps > 1000000000) {
-        for (let i = 0; i < endKeys.length; i++) {
-          if (endKeys[i] == currentKeys[i]) {
-            matchCount++;
-          }
-        }
-        if (matchCount == endKeys.length) {
-          goalFound = true;
-          console.log("\n\n\n\n GOAL FOUND !!!");
-          console.log(`Steps: ${steps}`);
-        }
+      if (currentNode.endsWith("Z")) {
+        goalFound = true;
       }
-    }
-    if (steps % 100000 == 0) {
-      console.log(steps);
     }
   }
-  //return steps;
+  return steps;
+}
+
+function calculateLCM(stepList) {
+  if (stepList.length === 0) {
+    return null;
+  }
+  let result = stepList[0];
+  for (let i = 1; i < stepList.length; i++) {
+    result = lcm(result, stepList[i]);
+  }
+  return result;
+}
+
+function gcd(a, b) {
+  // Implement GCD calculation (for example, using Euclidean algorithm)
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+function lcm(a, b) {
+  // LCM formula: LCM(a, b) = (a * b) / GCD(a, b)
+  return (a * b) / gcd(a, b);
 }
