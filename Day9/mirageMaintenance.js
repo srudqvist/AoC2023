dfbe3fb33a05cb8ca9fad7a473d27d6d62159ffb
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-24 10:27:36 -0600

    Day 9 part 1 refactoring

--- Day9/mirageMaintenance.js
+++ Day9/mirageMaintenance.js
@@ -5,4 +5,23 @@ let lines = fs.readFileSync("./testInput.txt").toString().trim().split("\n");
 lines.forEach((line, index) => {
   lines[index] = line.split(" ").map((c) => parseInt(c, 10));
 });
-console.log(lines);
+
+let sumValues = 0;
+
+for (const line in lines) {
+  console.log(lines[line]);
+  let allDiffs = [];
+  allDiffs.push(getDifferences(line));
+}
+
+function getDifferences(line) {
+  let differences = [];
+  for (let i = 0; i < lines[line].length - 1; i++) {
+    console.log(lines[line][i]);
+    let difference = lines[line][i + 1] - lines[line][i];
+    console.log(`Difference: ${difference}`);
+    differences.push(difference);
+    console.log(differences);
+  }
+  return differences;
+}
